2730ef61f8888230deadca79411baad7ab9adc74
Mayara Omai <mayara.omai@senai.sp.br>
2024-06-13 11:37:12 -0300

    Tela acervo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const ejs = require('ejs');
 const path = require('path');
+const { error } = require('console');
 
 const app = express();
 const port = 3000;
@@ -34,6 +35,85 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get(['/', '/home'], (req, res) => {
   res.render('home');
+});
+
+app.get('/acervo', (req, res) => {
+  db.query('select a.nome as autor, l.titulo, l.ISBN as ISBN, l.ano_publicacao from livro l join autor a on l.id_autor = a.id_autor', (error, results) => {
+    if (error){
+      console.log('Houve um erro ao recuperar os livros!', error)
+    }else{
+      res.render('acervo', { livros: results})
+    }
+  })
+});
+
+app.get('/pesquisarLivros', (req, res) => {
+  const pesquisa = req.query.pesquisa;
+  db.query('select a.nome as autor, l.titulo, l.ISBN as ISBN, l.ano_publicacao from livro l join autor a on l.id_autor = a.id_autor where l.titulo like ? or a.nome like ?', [`%${pesquisa}%`, `%${pesquisa}%`], (error, results) => {
+    if(error){
+      console.log('Ocorreu um erro ao realizar o filtro')
+    }else{
+      res.render('acervo', { livros: results})
+    }
+  });
+});
+
+const carregarAutores = (callback) => {
+  db.query('select * from autor order by nome', (error, results)=>{
+    if(error){
+      console.log('Erro ao carregar autores', error);
+    } else{
+      const autores = results.map(result => result)
+      callback(null, autores);
+    }
+  });
+} 
+
+app.get('/livro', (req, res) => {
+  const ISBN = req.query.ISBN;
+  console.log(ISBN)
+  carregarAutores((error, listaAutores) =>{
+    db.query('Select * from livro where ISBN=?', [ISBN], (error, results)=>{
+      if(error){
+        console.log('Erro ao buscar o livro com ISBN', ISBN)
+      }else{
+        if(results.length > 0){
+          res.render('livro', {autores: listaAutores, livro: results[0]});
+        }else{
+          console.log('Livro não encontrado.')
+        }
+      }
+    })
+  })
+})
+
+app.post('/editarLivro', (req, res)=>{
+  const ISBN = parseInt(req.body.inputISBN);
+  const id_autor = parseInt(req.body.inputAutor);
+  const titulo = req.body.inputTitulo;
+  const ano_publicacao = parseInt(req.body.inputAnoPublicacao);
+  const genero = req.body.inputGenero;
+  const resumo = req.body.textResumo;
+
+  db.query('update livro set ISBN = ?, titulo = ?, id_autor = ?, ano_publicacao = ?, genero = ?, resumo = ? where ISBN = ?', [ISBN, titulo, id_autor, ano_publicacao, genero, resumo, ISBN], (error, results) => {
+    if (error){
+      console.log('Erro ao editar Livro.')
+    }else{
+      res.redirect('/acervo')
+    }
+  })
+})
+
+app.post('/excluirLivro/:ISBN',(req, res) => {
+  const ISBN = parseInt(req.params.ISBN)
+  console.log(ISBN)
+  db.query('delete from livro where ISBN = ?', [ISBN], (error, results)=>{
+    if(error){
+      console.log('Erro ao excluir livro', error)
+    }else{
+      res.redirect('/acervo')
+    }
+  })
 });
 
 app.listen(port, () => {
